850c1aa9865fb5311aff3f83d6c1d7281b0e4569
h-mayorquin <h.mayorquin@gmail.com>
2015-09-03 10:55:27 +0200

    Fourth and fifith exercise

---
+++ 04_exercise/ex4.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/* Warning this program is wrong in purpose */
+
+int main(){
+  
+  int age = 10;
+  int height = 88;
+
+  printf("I am %d years old \n", age);
+  printf("I am %d inches tall", height);
+
+
+  return 0;
+}
